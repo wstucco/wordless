df4a8a988913c6f74055ede2c4be5456ebbc6b73
Massimo Ronca <massimo@mikamai.com>
2013-05-17 20:40:16 +0200

    added DirHelper class

---
+++ wordless/helpers/dir_helper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This module provides methods for accesing wordpress paths.
+ *
+ * @ingroup helperclass
+ */
+
+class DirHelper {
+
+  /**
+   * Returns the physical path for the specified file in the plugins directory.
+   * 
+   * @param string $path
+   *   The relative path inside the @e plugins/ folder.
+   * @return string
+   *   The complete path for the specified file/folder.
+   * 
+   * @ingroup helperfunc
+   */
+  function plugin_dir($path) {
+    return Wordless::join_paths(WP_CONTENT_DIR , $path);
+  }
+
+}
+
+Wordless::register_helper("DirHelper");
